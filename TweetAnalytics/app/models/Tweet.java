778f973ce83e9e7b2591b95994d2a482779856ed
RaghavDutta <rghvdutta007@gmail.com>
2018-03-13 02:41:39 -0400

    JavaDoc completed for model class

--- TweetAnalytics/app/models/Tweet.java
+++ TweetAnalytics/app/models/Tweet.java
@@ -1,8 +1,22 @@
+/**
+ * Model Package holds tweet's specific data
+ * 
+ */
 package models;
+/**
+ * This class acts as a model to store data which gets tweets from the user when text is added in the search bar
+ * @author raghav
+ * @version 2.0
+ */
+public class Tweet {
 
-public class Tweet 
-{
+	/**
+	 * stores name of user
+	 */
 	public String user;
+	/**
+	 * stores keyword 
+	 */
 	public String text;
 	
 public Tweet(String user,  String text) {
